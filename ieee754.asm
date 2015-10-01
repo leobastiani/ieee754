866f59705f12b29aec3415137562a26c6545ea3c
fabiosumida <dnxktzy@hotmail.com>
2015-10-01 02:18:47 -0300

    Update ieee754.asm

--- ieee754.asm
+++ ieee754.asm
@@ -44,7 +44,18 @@
 #######################################################
 
 	
-	main:
+main: # rotulo para ponto de entrada no processo
+
+
+
+
+	#######################################################
+	# aqui o programa deve chamar a função de bem-vindo
+	# ler as entradas do teclado e trata-las
+	# em seguida deve chamar a função intPartsToFloat e printBinHex
+	# parte do Fábio
+	#######################################################
+
 		li $v0, 4	#imprimir string
 		la $a0, menu
 		syscall
@@ -60,8 +71,10 @@
 		jal ler_string		#le a string
 		lb $t1, buffer
 		
-		bne $t1, 43, verifica_menos	#verirfica se eh "+" ou "-"
+		bne $t1, 43, verifica_semenos	#verirfica se eh "+" ou "-"
 		beq $t1, 43, se_mais
+		
+		eh_menos:
 		beq $t1, 45, se_menos
 		
 		voltamain:
@@ -69,20 +82,39 @@
 		li $s0, 0		#s0=0 s0->posicao da string
 		li $a0, 0		#a1=0 a0->valor inteiro
 		li $a1, 0		#a2=0 a2->valor decimal
-		li $a2, 0		#numero de casas decimais
+		li $a2, 1		#numero que divide a parte decimal
 		jal loop_int
+		jal verifica_a2		#se a2 for igual a 1 na verdade ele deveria ser 0
 		
 		#CATCH-IN CONTINUA DAQUI
-				
+		
+		jal intPartsToFloat
+		
+		move $a0, $v0
+		
+		jal printBinHex
+
+		
 		j main
 		
+	verifica_a2:
+		beq $a2, 1, mudar_pra_zero
+		jr $ra
+		
+	mudar_pra_zero:
+		li $a2, 0
+		jr $ra
+		
+		
+		
 	se_mais:
-		li $a3, 1
+		li $a3, 0
 		j voltamain
 		
 	se_menos:
-		li $a3, 0
+		li $a3, 1
 		j voltamain
+		
 	loop_int:
 		addi $s0, $s0, 1
 		lb $t1, buffer($s0)
@@ -90,14 +122,21 @@
 		beq $t1, 44, loop_decimal	#verifica se eh virgula
 		blt $t1, 48, invalid		#verifica se eh um numero
 		bgt $t1, 57, invalid		#verifica se eh um numero
+
+		
 		mul $a0, $a0, 10		#aumenta uma casa decimal do valor anterior
+		
+
+		
 		addi $t1, $t1, -48		#transforma ascii em int
 		add $a0, $a0, $t1
+		
 		j loop_int
 		
-	verifica_menos:
+	verifica_semenos:
 		bne $t1, 45, invalid
-		jr $ra
+		j eh_menos
+		
 		
 	loop_decimal:
 		addi $s0, $s0, 1		#incrementa a posicao do "vetor"
@@ -106,7 +145,7 @@
 		blt $t1, 48, invalid		#verifica se eh um numero
 		bgt $t1, 57, invalid		#verifica se eh um numero
 		addi $t1, $t1, -48		#transforma ascii em int
-		addi $a2, $a2, 1		#soma o num de casas decimais
+		addi $a2, $a2, 10		#a2 = a2 * 10
 		mul $a1, $a1, 10		#aumenta uma casa decimal
 		add $a1, $a1, $t1		#soma com o proximo valor
 		j loop_decimal
@@ -149,9 +188,7 @@
 		
 	end:
 		li $v0, 10
-		syscall
-		
-		
+		syscall	
 	
 
 
